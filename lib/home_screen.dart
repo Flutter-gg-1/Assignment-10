afd41097eeaa537f5f645e92cc5b05bac5db3b22
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-08-12 19:40:51 +0300

    created container for user information

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,36 +11,107 @@ class HomeScreen extends StatelessWidget {
             child: Text('User Settings',
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold))),
       ),
-      body: Container(
-          padding: EdgeInsets.only(left: 20, top: 20),
-          height: 200,
-          width: 300,
-          color: Colors.blue,
-          child: Column(
-            children: [
-              Row(
+      body: Column(
+        children: [
+          Container(
+              padding: EdgeInsets.only(left: 20, top: 20),
+              height: 150,
+              width: 300,
+              color: const Color.fromARGB(255, 72, 133, 211),
+              child: Column(
                 children: [
-                  Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          color: Colors.white, shape: BoxShape.circle),
-                      child: Icon(
-                        Icons.account_circle_rounded,
-                      )),
-                  Column(
+                  Row(
                     children: [
-                      Text('data'),
-                      Text('data'),
+                      Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              color: Colors.white, shape: BoxShape.circle),
+                          child: Icon(
+                            Icons.account_circle_rounded,
+                          )),
+                      Column(
+                        children: [
+                          Text(
+                            'Abdulaziz Muslih Alamri',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text('Creative builder',
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 223, 221, 221),
+                                fontSize: 12,
+                              )),
+                        ],
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Column(
+                        children: [
+                          Text('846',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold)),
+                          Text('Collect',
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 223, 221, 221),
+                                fontSize: 12,
+                              )),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('51',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold)),
+                          Text('Attention',
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 223, 221, 221),
+                                fontSize: 12,
+                              )),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('267',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold)),
+                          Text('Track',
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 223, 221, 221),
+                                fontSize: 12,
+                              )),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('39',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold)),
+                          Text('Coupons',
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 223, 221, 221),
+                                fontSize: 12,
+                              )),
+                        ],
+                      ),
                     ],
                   )
                 ],
-              ),
-              Row(
-                children: [],
-              )
-            ],
-          )),
+              )),
+        ],
+      ),
     );
   }
 }
