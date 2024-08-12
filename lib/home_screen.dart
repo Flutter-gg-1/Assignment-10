5ed99ce058d9d581878834675faa8500273c72cb
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-08-12 21:10:21 +0300

    created UI for address,privacy,general,notification and support

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,7 +6,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 245, 245, 245),
       appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 245, 245, 245),
         title: Center(
             child: Text('User Settings',
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold))),
@@ -17,7 +19,10 @@ class HomeScreen extends StatelessWidget {
               padding: EdgeInsets.only(left: 20, top: 20),
               height: 150,
               width: 300,
-              color: const Color.fromARGB(255, 72, 133, 211),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color.fromARGB(255, 72, 133, 211),
+              ),
               child: Column(
                 children: [
                   Row(
@@ -30,6 +35,7 @@ class HomeScreen extends StatelessWidget {
                           child: Icon(
                             Icons.account_circle_rounded,
                           )),
+                      SizedBox(width: 10),
                       Column(
                         children: [
                           Text(
@@ -48,6 +54,7 @@ class HomeScreen extends StatelessWidget {
                       )
                     ],
                   ),
+                  SizedBox(height: 20),
                   Row(
                     children: [
                       Column(
@@ -110,60 +117,224 @@ class HomeScreen extends StatelessWidget {
                   )
                 ],
               )),
+          SizedBox(height: 20),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Column(
                 children: [
                   Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: const Color.fromARGB(255, 223, 223, 223)
-                    ),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: const Color.fromARGB(255, 223, 223, 223)),
                       child: IconButton(
-                          onPressed: () {}, icon: Icon(Icons.attach_money))),
+                          onPressed: () {},
+                          icon: Icon(Icons.attach_money, color: Colors.black))),
                   Text('Wallet')
                 ],
               ),
               Column(
                 children: [
-                       Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: const Color.fromARGB(255, 223, 223, 223)
-                    ),
+                  Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: const Color.fromARGB(255, 223, 223, 223)),
                       child: IconButton(
-                          onPressed: () {}, icon: Icon(Icons.card_giftcard))),
+                          onPressed: () {},
+                          icon:
+                              Icon(Icons.card_giftcard, color: Colors.black))),
                   Text('Delivery')
                 ],
               ),
               Column(
                 children: [
-                       Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: const Color.fromARGB(255, 223, 223, 223)
-                    ),
+                  Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: const Color.fromARGB(255, 223, 223, 223)),
                       child: IconButton(
-                          onPressed: () {}, icon: Icon(Icons.message_outlined))),
+                          onPressed: () {},
+                          icon: Icon(Icons.message, color: Colors.black))),
                   Text('Message')
                 ],
               ),
               Column(
                 children: [
-                       Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: const Color.fromARGB(255, 223, 223, 223)
-                    ),
+                  Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: const Color.fromARGB(255, 223, 223, 223)),
                       child: IconButton(
-                          onPressed: () {}, icon: Icon(Icons.room_service_sharp))),
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.room_service_sharp,
+                            color: Colors.black,
+                          ))),
                   Text('Service')
                 ],
               ),
             ],
           ),
-          
+          SizedBox(
+            height: 20,
+          ),
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(10)),
+                height: 60,
+                width: 350,
+                child: Row(
+                  children: [
+                    Container(
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: const Color.fromARGB(255, 133, 102, 183)),
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.white,
+                        )),
+                    Column(
+                      children: [
+                       Text(
+                            'Address',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text('Ensure your harvesting address',
+                              style: TextStyle(
+                                color:  const Color.fromARGB(255, 142, 141, 141),
+                                fontSize: 12,
+                              )),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10)),
+                height: 60,
+                width: 350,
+                child: Row(
+                  children: [
+                    Container(
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: const Color.fromARGB(255, 225, 73, 215)),
+                        child: Icon(
+                          Icons.lock,
+                          color: Colors.white,
+                        )),
+                    Column(
+                      children: [
+                        //Text Privacy
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10)),
+                height: 60,
+                width: 350,
+                child: Row(
+                  children: [
+                    Container(
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: const Color.fromARGB(255, 231, 179, 97)),
+                        child: Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        )),
+                    Column(
+                      children: [
+                        //Text General
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10)),
+                height: 60,
+                width: 350,
+                child: Row(
+                  children: [
+                    Container(
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: const Color.fromARGB(255, 65, 201, 219)),
+                        child: Icon(
+                          Icons.notifications,
+                          color: Colors.white,
+                        )),
+                    Column(
+                      children: [
+                        //Text Notification
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10)),
+                height: 60,
+                width: 350,
+                child: Row(
+                  children: [
+                      Container(
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: const Color.fromARGB(255, 201, 183, 162)),
+                        child: Icon(
+                          Icons.chat_bubble_outlined,
+                          color: Colors.white,
+                        )),
+                    Column(
+                      children: [
+                        //Text Support
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
