0020b3ca6fe2948941bd70a05867c1544543c109
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-12 20:02:33 +0300

    add options

--- lib/page/user_setting_screen.dart
+++ lib/page/user_setting_screen.dart
@@ -5,18 +5,87 @@ class UserSettingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromRGBO(250, 248, 249, 1),
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(250, 248, 249, 1),
       body: SafeArea(
-        child: Center(
-          child: Column(
+        child: Container(
+          margin: const EdgeInsets.all(18.0),
+          child: const Column(
             children: [
-              Text("User Settings"),
+              Text(style: TextStyle(fontSize: 30), "User Settings"),
               TitleCard(),
+              Spacer(),
+              QuickOptions(),
+              Spacer()
+
+              Card()
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class QuickOptions extends StatelessWidget {
+  const QuickOptions({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(1, 243, 243, 248),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Icon(Icons.monetization_on_outlined),
+            ),
+            const Text("Wallet")
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(1, 243, 243, 248),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Icon(Icons.card_giftcard),
+            ),
+            const Text("Delivery")
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(1, 243, 243, 248),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Icon(Icons.message),
+            ),
+            const Text("Message")
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(1, 243, 243, 248),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Icon(Icons.room_service),
+            ),
+            const Text("Service")
+          ],
+        ),
+      ],
     );
   }
 }
@@ -28,46 +97,62 @@ class TitleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
-      color: Color.fromRGBO(56, 116, 254, 1),
+    return Container(
+      height: 180,
+      decoration: BoxDecoration(
+        boxShadow: kElevationToShadow[2],
+        color: const Color.fromRGBO(56, 116, 254, 1),
+        borderRadius: BorderRadius.circular(20),
+      ),
       child: Column(
         children: [
-          ListTile(
-            leading: CircleAvatar(child: Icon(Icons.account_circle)),
-            title: Text(
-                style: TextStyle(color: Colors.white, fontSize: 24),
-                'Rein Gundersen Bentdal'),
-            subtitle:
-                Text(style: TextStyle(color: Colors.white), 'Creative buildee'),
+          Container(
+            margin: const EdgeInsets.all(8.0),
+            height: 100,
+            child: const ListTile(
+              leading: CircleAvatar(child: Icon(Icons.account_circle)),
+              title: Text(
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  'Rein Gundersen Bentdal'),
+              subtitle: Text('Creative buildee'),
+            ),
           ),
-          Row(
+          const Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Column(
                 children: [
-                  Text(style: TextStyle(color: Colors.white), "846"),
-                  Text(style: TextStyle(color: Colors.white), "Collect"),
+                  Text(
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      "846"),
+                  Text("Collect"),
                 ],
               ),
               Column(
                 children: [
-                  Text(style: TextStyle(color: Colors.white), "51"),
-                  Text(style: TextStyle(color: Colors.white), "Attention"),
+                  Text(
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      "51"),
+                  Text("Attention"),
                 ],
               ),
               Column(
                 children: [
-                  Text(style: TextStyle(color: Colors.white), "267"),
-                  Text(style: TextStyle(color: Colors.white), "Track"),
+                  Text(
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      "267"),
+                  Text("Track"),
                 ],
               ),
               Column(
                 children: [
-                  Text(style: TextStyle(color: Colors.white), "39"),
-                  Text(style: TextStyle(color: Colors.white), "Coupons"),
+                  Text(
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      "39"),
+                  Text("Coupons"),
                 ],
               ),
-              ],
+            ],
           )
         ],
       ),
