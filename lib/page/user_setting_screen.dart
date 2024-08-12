7d56a5d629186b34b99cb458bc34dc5771738a11
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-12 17:25:20 +0300

    create page for user setting

---
+++ lib/page/user_setting_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UserSettingScreen extends StatelessWidget {
+  const UserSettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
