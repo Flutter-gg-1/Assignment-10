4c87b3e61bfa3e709e4e0148e940132e671fd802
muhannad <mohndalzaher@gmail.com>
2024-08-13 03:50:44 +0300

    finshed from project and completed

--- lib/src/firstScrean.dart
+++ lib/src/firstScrean.dart
@@ -7,8 +7,8 @@ class Firstscrean extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "user Sittings",
+        title: const Text(
+          "User sittings",
           style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
         ),
       ),
@@ -20,6 +20,13 @@ class Firstscrean extends StatelessWidget {
               height: 200,
               width: 400,
               decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.blue,
+                    blurRadius: 10.0,
+                    offset: Offset(0, 4),
+                  )
+                ],
                 color: Colors.blue,
                 borderRadius: BorderRadius.circular(20),
               ),
@@ -38,52 +45,94 @@ class Firstscrean extends StatelessWidget {
                       style: TextStyle(color: Colors.white, fontSize: 30),
                     ),
                     subtitle: Text("Flutter boot camp",
-                        style: TextStyle(color: Colors.grey, fontSize: 17)),
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 221, 216, 216),
+                            fontSize: 17)),
                   ),
                   SizedBox(
-                    height: 20,
+                    height: 30,
                   ),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Expanded(
-                        child: ListTile(
-                          title: Text("846",
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "846",
                               style:
-                                  TextStyle(color: Colors.white, fontSize: 25)),
-                          subtitle: Text("collect",
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 15)),
+                                  TextStyle(color: Colors.white, fontSize: 25),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              "collect",
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 221, 216, 216),
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                       Expanded(
-                        child: ListTile(
-                          title: Text("51",
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "51",
                               style:
-                                  TextStyle(color: Colors.white, fontSize: 25)),
-                          subtitle: Text("Attention",
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 13)),
+                                  TextStyle(color: Colors.white, fontSize: 25),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              "Attention",
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 221, 216, 216),
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                       Expanded(
-                        child: ListTile(
-                          title: Text("267",
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "267",
                               style:
-                                  TextStyle(color: Colors.white, fontSize: 25)),
-                          subtitle: Text("track",
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 15)),
+                                  TextStyle(color: Colors.white, fontSize: 25),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              "track",
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 221, 216, 216),
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                       Expanded(
-                        child: ListTile(
-                          title: Text("39",
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "39",
                               style:
-                                  TextStyle(color: Colors.white, fontSize: 25)),
-                          subtitle: Text("copuns",
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 15)),
+                                  TextStyle(color: Colors.white, fontSize: 25),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              "coupons",
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 221, 216, 216),
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
@@ -91,7 +140,7 @@ class Firstscrean extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
             const Row(
@@ -123,7 +172,7 @@ class Firstscrean extends StatelessWidget {
                       radius: 35,
                       backgroundColor: Color.fromARGB(255, 181, 192, 198),
                       child: Icon(
-                        Icons.delivery_dining,
+                        Icons.shop,
                         size: 35,
                         color: Colors.black,
                       ),
@@ -174,7 +223,112 @@ class Firstscrean extends StatelessWidget {
                   ],
                 ),
               ],
-            )
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: ListTile(
+                  title: Text(
+                    "Address",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.purple,
+                    child: Icon(
+                      size: 30,
+                      Icons.add_location,
+                      color: Colors.white,
+                    ),
+                  ),
+                  subtitle: Text("Ensure your harvesting address"),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: ListTile(
+                  title: Text(
+                    "Privacy",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Color.fromARGB(255, 229, 58, 124),
+                    child: Icon(
+                      size: 30,
+                      Icons.lock,
+                      color: Colors.white,
+                    ),
+                  ),
+                  subtitle: Text("System permsion change"),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: ListTile(
+                  title: Text(
+                    "Ginral",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Color.fromARGB(238, 198, 164, 28),
+                    child: Icon(
+                      size: 30,
+                      Icons.menu,
+                      color: Colors.white,
+                    ),
+                  ),
+                  subtitle: Text("Ensure your harvesting address"),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: ListTile(
+                  title: Text(
+                    "Notficaions",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Color.fromARGB(255, 38, 210, 181),
+                    child: Icon(
+                      size: 30,
+                      Icons.notifications,
+                      color: Colors.white,
+                    ),
+                  ),
+                  subtitle: Text("Basic functioal settings"),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: ListTile(
+                  title: Text(
+                    "Support",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Color.fromARGB(255, 136, 114, 94),
+                    child: Icon(
+                      Icons.chat,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  ),
+                  subtitle: Text("we are here to healp"),
+                ),
+              ),
+            ),
           ],
         ),
       ),
