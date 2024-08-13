0972dae3bb34efcf9acb8db9e83704f383e799e0
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-13 07:04:34 +0300

    UPD Colores

--- lib/location_and_others.dart
+++ lib/location_and_others.dart
@@ -1,207 +1,212 @@
 import 'package:flutter/material.dart';
 
 Widget locationAndOthers() {
-    return Column(
-      children: [
-        Container(
-          height: 100,
-          width: 400,
-          color: Colors.blue[800],
-          alignment: Alignment.centerLeft,
-          child: const Padding(
-            padding: EdgeInsets.only(left: 20),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                    Icons.location_pin,
-                    color: Colors.black,
-                    size: 40,
+  return Column(
+    children: [
+      Container(
+        height: 100,
+        width: 400,
+        color: Colors.blue[800],
+        alignment: Alignment.centerLeft,
+        child: const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.location_pin,
+                  color: Colors.black,
+                  size: 40,
+                ),
+              ),
+              SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Address',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
                   ),
-                ),
-                SizedBox(width: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Address',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Ensure you harvesting address',
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                  Text(
+                    'Ensure you harvesting address',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-        const SizedBox(height: 20),
-        Container(
-          height: 100,
-          width: 400,
-          color: Colors.blue[800],
-          alignment: Alignment.centerLeft,
-          child: const Padding(
-            padding: EdgeInsets.only(left: 20),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                    Icons.privacy_tip_outlined,
-                    color: Colors.black,
-                    size: 40,
+      ),
+      const SizedBox(height: 20),
+      Container(
+        height: 100,
+        width: 400,
+        color: Colors.blue[800],
+        alignment: Alignment.centerLeft,
+        child: const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.privacy_tip_outlined,
+                  color: Colors.black,
+                  size: 40,
+                ),
+              ),
+              SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Privacy',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
                   ),
-                ),
-                SizedBox(width: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Privacy',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'System permission changes',
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                  Text(
+                    'System permission changes',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-        const SizedBox(height: 20),
-        Container(
-          height: 100,
-          width: 400,
-          color: Colors.blue[800],
-          alignment: Alignment.centerLeft,
-          child: const Padding(
-            padding: EdgeInsets.only(left: 20),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                    Icons.g_mobiledata_rounded,
-                    color: Colors.black,
-                    size: 40,
+      ),
+      const SizedBox(height: 20),
+      Container(
+        height: 100,
+        width: 400,
+        color: Colors.blue[800],
+        alignment: Alignment.centerLeft,
+        child: const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.g_mobiledata_rounded,
+                  color: Colors.black,
+                  size: 40,
+                ),
+              ),
+              SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'General',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
                   ),
-                ),
-                SizedBox(width: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'General',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Basic functional settings',
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                  Text(
+                    'Basic functional settings',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-        const SizedBox(height: 20),
-        Container(
-          height: 100,
-          width: 400,
-          color: Colors.blue[800],
-          alignment: Alignment.centerLeft,
-          child: const Padding(
-            padding: EdgeInsets.only(left: 20),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                    Icons.alarm,
-                    color: Colors.black,
-                    size: 40,
+      ),
+      const SizedBox(height: 20),
+      Container(
+        height: 100,
+        width: 400,
+        color: Colors.blue[800],
+        alignment: Alignment.centerLeft,
+        child: const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.alarm,
+                  color: Colors.black,
+                  size: 40,
+                ),
+              ),
+              SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Notifications',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
                   ),
-                ),
-                SizedBox(width: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Notifications',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Take over the news in time',
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                  Text(
+                    'Take over the news in time',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-        const SizedBox(height: 20),
-        Container(
-          height: 100,
-          width: 400,
-          color: Colors.blue[800],
-          alignment: Alignment.centerLeft,
-          child: const Padding(
-            padding: EdgeInsets.only(left: 20),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                    Icons.support_agent,
-                    color: Colors.black,
-                    size: 40,
+      ),
+      const SizedBox(height: 20),
+      Container(
+        height: 100,
+        width: 400,
+        color: Colors.blue[800],
+        alignment: Alignment.centerLeft,
+        child: const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.support_agent,
+                  color: Colors.black,
+                  size: 40,
+                ),
+              ),
+              SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Support',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
                   ),
-                ),
-                SizedBox(width: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Support',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'we are here to help',
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                  Text(
+                    'we are here to help',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-      ],
-    );
-  }
+      ),
+    ],
+  );
+}
