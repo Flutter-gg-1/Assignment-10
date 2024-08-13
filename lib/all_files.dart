711ad8399ec5d8ae9a8019caa23d8dce5d0aeffa
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-13 07:08:34 +0300

    UPD widgets

--- lib/all_files.dart
+++ lib/all_files.dart
@@ -1,3 +1,3 @@
-export 'container_info.dart';
-export 'row_w_d_m_service.dart';
-export 'location_and_others.dart';
+export 'widgets/container_info.dart';
+export 'widgets/row_w_d_m_service.dart';
+export 'widgets/location_and_others.dart';
